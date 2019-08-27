13fb9371763ffa8a5072def17dc39fb0fadc4cb2
jumana15 <53777750+jumana15@users.noreply.github.com>
2019-08-27 12:32:06 +0300

    Add files via upload

---
+++ DS/GenericHash/hash.h
@@ -0,0 +1,27 @@
+#ifndef HASH_H
+#define HASH_H
+
+typedef struct Node* hashNode;
+
+typedef size_t (*hashFunc)(void* s);
+typedef int (*cmpFunc)(void* a,void* b);
+typedef void (*printFunc)(void* a,void* b);
+
+typedef struct{
+	hashNode* buckets;
+	size_t size;
+}hashArr;
+
+hashArr* createHash(size_t size);
+
+void destroyHash(hashArr* h);
+
+int insertHash(hashArr* h,void* key,void* val,hashFunc hf,cmpFunc cmpf);
+
+int deleteHash(hashArr* h,void* key,hashFunc hf,cmpFunc cmpf);
+
+int findHash(hashArr* h,hashNode* prev,hashNode* cur,void* key,hashFunc hf,cmpFunc cmpf);
+
+void forEachHash(hashArr* h,printFunc pf);
+
+#endif
