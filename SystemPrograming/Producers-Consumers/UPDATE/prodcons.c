ba58abd5fcb1958270d624a45738dcb3fb1f3552
jumana15 <53777750+jumana15@users.noreply.github.com>
2019-09-10 18:59:25 +0300

    Update prodcons.c

--- SystemPrograming/Producers-Consumers/UPDATE/prodcons.c
+++ SystemPrograming/Producers-Consumers/UPDATE/prodcons.c
@@ -9,33 +9,35 @@
 void createQ2(Q2** q2,size_t size)
 {
 	(*q2)=(Q2*)malloc(sizeof(Q2));
-
 	if(!(*q2))
 		exit(1);
 	createQueue(&((*q2)->buffer),size);
-    createQueue(&((*q2)->bp),size);
+    	createQueue(&((*q2)->bp),size);
 
+}
+void destroyQ2(Q2* q2)
+{
+	if(!q2)
+		return;
+	destroyQueue(q2->buffer);
+    	destroyQueue(q2->bp);		
+	free(q2);
 }
 int main()
 {
 	Q2* q2;
 	producersStruct* ps;
 	consumersStruct* cn;
-    int status;
-    size_t i,j;
-    char* s;
-    size_t size=10;
-    createQ2(&q2,size);
-    count=97;
-    count2=48;
-    createProducers(&ps,3);
-    createConsumers(&cn,3);
-    insertProducers(q2,ps);
-    deleteConsumers(q2,cn);
-    joinProducers(ps);
-    joinConsumers(cn);
-    /*destroy*/
-
-
-  return 0;
+    	size_t size=10;
+	count=97;
+    	count2=48;
+    	createQ2(&q2,size);
+    	createProducers(&ps,3);
+    	createConsumers(&cn,3);
+    	insertProducers(q2,ps);
+    	deleteConsumers(q2,cn);
+    	joinProducers(ps);
+    	joinConsumers(cn);
+	destroyQ2(q2);
+  	return 0;
 }
