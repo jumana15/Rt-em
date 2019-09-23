394a48b8ee56f557322d5d9153349d317ad35e7a
jumana15 <53777750+jumana15@users.noreply.github.com>
2019-09-23 20:11:54 +0300

    Add files via upload

---
+++ C++/StringsPart2/stringt.h
@@ -0,0 +1,45 @@
+#ifndef STRINGT_H
+#define STRINGT_H
+#include <iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+class String
+{
+	public:
+		String();
+		String(const char* str);
+		String(const String& str);
+		String& operator=(const String& str);
+		size_t length() const;
+		void setString(const char* str);
+		const char* getString() const;
+		int compare(const String& str);
+		void print() const;
+		void upperCase();
+		void lowerCase();
+		void prepend(const String& str);
+		void prepend(const char* str);
+		String& operator+=(const String& str);
+		String& operator+=(const char* str);
+		int operator<(const String& str) const;
+		int operator>(const String& str) const;
+		int operator<=(const String& str) const;
+		int operator>=(const String& str) const;
+		int operator==(const String& str) const;
+		int operator!=(const String& str) const;
+		int contain(const String& str) const;
+		char& operator[](size_t index);
+		char operator[](size_t index) const;
+		~String();
+	private:
+		char* str;
+		size_t len;
+
+};
+
+ostream& operator<<(ostream& os,const String& str);
+istream& operator>>(istream& is,String& str);
+
+#endif
