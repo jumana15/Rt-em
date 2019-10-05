bac0539c5aedbcbcf52a1310bc66624098bc5894
jumana15 <53777750+jumana15@users.noreply.github.com>
2019-10-06 01:41:32 +0300

    Add files via upload

---
+++ C++/CITY/FINAL/city.h
@@ -0,0 +1,94 @@
+#ifndef CITY_H
+#define CITY_H
+
+#include<stdio.h>
+#include"building.h"
+#include "street.h"
+
+template<class C,class S,class B> 
+class City_t
+{
+	private:
+		C m_cityID;
+		vector<Street_t<S,B>*> m_streetsVec;
+	public:
+		City_t();
+		City_t(const City_t& c);
+		City_t<C,S,B>& operator=(const City_t& c);
+		inline void setCityID(const C& cityID);
+		inline const C& getCityID() const;
+		void AddStreet(const Street_t<S,B>* s);
+		const Street_t<S,B>* getStreet(int index) const;
+		const vector<Street_t<S,B>*> getVec() const;
+		~City_t();
+};
+
+template<class C,class S,class B>
+City_t<C,S,B>::City_t(){}
+
+template<class C,class S,class B>
+City_t<C,S,B>::City_t(const City_t& c)
+{
+	vector<Street_t<S,B>*> v=c.getVec(); 
+	this->m_cityID=c.getCityID();
+	for(int i=0;i<v.size();i++)
+	{
+		m_streetsVec.AddStreet(v[i]);
+	}
+}
+
+template<class C,class S,class B>
+inline void City_t<C,S,B>::setCityID(const C& cityID){ this->m_cityID=cityID; }
+
+template<class C,class S,class B>
+inline const C& City_t<C,S,B>::getCityID() const{ return this->m_cityID; }
+
+template<class C,class S,class B>
+void City_t<C,S,B>::AddStreet(const Street_t<S,B>* s){ m_streetsVec.push_back((Street_t<S,B>*)s); }
+
+template<class C,class S,class B> 
+const Street_t<S,B>* City_t<C,S,B>::getStreet(int index) const
+{
+	if(index>=m_streetsVec.size())
+	{
+		throw("IndexOutOfBounds!");
+	}
+	return m_streetsVec[index];
+}
+
+template<class C,class S,class B> 
+const vector<Street_t<S,B>*> City_t<C,S,B>::getVec() const{ return m_streetsVec; }
+
+template<class C,class S,class B>
+City_t<C,S,B>::~City_t()
+{
+	for(int i=0;i<m_streetsVec.size();i++)
+		{
+			delete m_streetsVec[i];
+		}
+		m_streetsVec.clear();
+}
+
+template<class C,class S,class B>
+City_t<C,S,B>& City_t<C,S,B>::operator=(const City_t& c)
+{
+	if(this!=&c)
+	{
+		vector<Street_t<S,B>*> v=c.getVec(); 
+		Street_t<S,B>* s;
+		for(int i=0;i<m_streetsVec.size();i++)
+		{
+			delete m_streetsVec[i];
+		}
+		m_streetsVec.clear();
+		this->m_cityID=c.getCityID();
+		for(int i=0;i<v.size();i++)
+		{
+			s=new Street_t<S,B>;
+			*s=*v[i];
+			m_streetsVec.AddStreet(s);
+		}
+	}
+	return *this;
+}
+#endif
---
+++ C++/CITY/FINAL/street.h
@@ -0,0 +1,95 @@
+#ifndef STREET_H
+#define STREET_H
+
+#include<stdio.h>
+#include<vector>
+#include "building.h"
+
+template<class S,class B> 
+class Street_t
+{
+	private:
+		S m_steetID;
+		vector<Building_t<B>*> m_buildingsVec;
+
+	public:
+		Street_t();
+		Street_t(const Street_t& s);
+		Street_t& operator=(const Street_t& s);
+		inline void setStreetID(const S& streetID);
+		inline const S& getStreetID() const;
+		void AddBuilding2Street(const Building_t<B>* b);
+		const Building_t<B>* getBuilding(int index) const;
+		const vector<Building_t<B>*> getVec() const;
+		~Street_t();
+};
+
+template<class S,class B> 
+Street_t<S,B>::Street_t(){}
+
+template<class S,class B> 
+Street_t<S,B>::Street_t(const Street_t& s)
+{
+	vector<Building_t<B>*> v=s.getVec(); 
+	this->m_steetID=s.getStreetID();
+	for(int i=0;i<v.size();i++)
+	{
+		m_buildingsVec.AddBuilding2Street(v[i]);
+	}
+}
+
+template<class S,class B> 
+Street_t<S,B>& Street_t<S,B>::operator=(const Street_t<S,B>& s)
+{
+	if(this!=&s)
+	{
+		vector<Building_t<B>*> v=s.getVec();
+		Building_t<B>* b;
+		for(int i=0;i<m_buildingsVec.size();i++)
+		{
+			delete m_buildingsVec[i];
+		} 
+		m_buildingsVec.clear();
+		this->m_steetID=s.getStreetID();
+		for(int i=0;i<v.size();i++)
+		{
+			b=new Building_t<B>;
+			*b=*v[i];
+			m_buildingsVec.AddBuilding2Street(b);
+		}
+		return *this;
+	}
+}
+
+template<class S,class B> inline void Street_t<S,B>::setStreetID(const S& streetID){ this->m_steetID=streetID; }
+
+template<class S,class B> 
+inline const S& Street_t<S,B>::getStreetID() const { return this->m_steetID; }
+
+template<class S,class B> 
+void Street_t<S,B>::AddBuilding2Street(const Building_t<B>* b){ m_buildingsVec.push_back((Building_t<B>*)b); }
+
+template<class S,class B> 
+const Building_t<B>* Street_t<S,B>::getBuilding(int index) const
+{
+	if(index>=m_buildingsVec.size())
+	{
+		throw("IndexOutOfBounds!");
+	}
+	return m_buildingsVec[index];
+}
+
+template<class S,class B> 
+const vector<Building_t<B>*> Street_t<S,B>::getVec() const { return m_buildingsVec; }
+
+template<class S,class B> 
+Street_t<S,B>::~Street_t()
+{
+	for(int i=0;i<m_buildingsVec.size();i++)
+	{
+		delete m_buildingsVec[i];
+	}
+	m_buildingsVec.clear();
+}
+
+#endif
